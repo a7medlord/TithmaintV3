616e299067b522ef37177fde8fa1d45637e8a3ad
mooaz <ma3az@gmail.com>
2017-02-23 14:24:37 +0300

    MasterDetilsIncreate

--- src/CloudApp/Controllers/QuotationsController.cs
+++ src/CloudApp/Controllers/QuotationsController.cs
@@ -56,7 +56,7 @@ namespace CloudApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Bank,Complate,CustmerId,FBatch,QDate,SCustmer,Sign")] Quotation quotation)
+        public async Task<IActionResult> Create([Bind] Quotation quotation)
         {
             if (ModelState.IsValid)
             {
