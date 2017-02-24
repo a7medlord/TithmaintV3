645a48610a167b5c955408aea8c890305c1456d8
a7medlord <johndoe@example.com>
2017-02-24 08:12:40 +0300

    change ip

--- src/CloudApp/Program.cs
+++ src/CloudApp/Program.cs
@@ -10,7 +10,7 @@ namespace CloudApp
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseUrls("http://192.168.1.3:5000")
+                .UseUrls("http://192.168.1.3:50001")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
