cde5928d7788d6cabf4b91734902e7bc25af5d5c
Moaaz <Devloper@TalaGroups.com>
2017-03-28 19:54:01 +0300

    ser

---
+++ src/CloudApp/Migrations/20170328162507_migr8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CloudApp.Migrations
+{
+    public partial class migr8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
