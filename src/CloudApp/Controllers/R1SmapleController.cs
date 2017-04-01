36f801080169c60c7a88b30616db40e0da6f25d1
a7medlord <johndoe@example.com>
2017-04-02 02:00:06 +0300

    add report

--- src/CloudApp/Controllers/R1SmapleController.cs
+++ src/CloudApp/Controllers/R1SmapleController.cs
@@ -11,6 +11,7 @@ using CloudApp.Models;
 using CloudApp.Models.BusinessModel;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Reporting.WebForms;
 
 namespace CloudApp.Controllers
 {
@@ -28,8 +29,97 @@ namespace CloudApp.Controllers
             _userManager = usermanger;
             _env = env;
         }
-        
-      
+
+        public IActionResult GetSample1Report()
+        {
+
+            byte[] rendervalue = GetSample1ReportasStreem();
+
+            return File(rendervalue, "application/pdf");
+        }
+
+        public byte[] GetSample1ReportasStreem()
+        {
+            ReportDataSource reportDataSource = new ReportDataSource();
+
+            // get attachment  
+            //var attament = _context.AttachmentForTreaments.Where(d => d.TreatmentId == id);
+            //string images = null;
+            //foreach (var attachmentForTreament in attament)
+            //{
+            //    images += "http://" + HttpContext.Request.Host + "/sample1attachment/" + attachmentForTreament.AttachmentId + ".jpg" + ",";
+            //}
+
+            List<R1Smaple> sample = new List<R1Smaple>()
+           {
+               new R1Smaple(){Id = 1  ,LastTaqeem = 455,InterfcaesEast = "ÏåÇä",InterfcaesWest= "ÏåÇä",InterfcaesSouth = "ÏåÇä",InterfcaesNorth = "ÏåÇä",MarkterRoad = "ÎÇáÏ Èä ÇáæáíÏ",Owner = "ãÍãÏ Úáí ÇáãÓÊæÑ",South = "ÔÇÑÚ 30",SouthTall = "22 ã",West = "ÔÇÑÚ 30",WestTall = "22 ã",East = "ÔÇÑÚ 30",EastTall = "22 ã",North = "ÔÇÑÚ 30",NorthTall = "22 ã",IsDonForSndElectric =true,IslAder =true ,BuldingNumber = "232444",IsDoublWall =true ,IsDoublGlass = true,BulState ="ãåÊÑÆ", AqarType = "ÝíáÇ", AqarScope = "äØÇÞ ÇáÚÞÇÑ" , Mansob = "ãáí" }
+
+
+           };
+
+            // Qoution Report
+            //var treatments = _context.Treatment.Include(d => d.Custmer).ThenInclude(s => s.Sample).Where(d => d.Id == id);
+            reportDataSource.Name = "DataSetR1Sample";
+            reportDataSource.Value = sample;
+            //string custmer = treatments.SingleOrDefault()?.Custmer.Name;
+            //string sample = treatments.SingleOrDefault()?.Custmer.Sample.Name;
+            LocalReport local = new LocalReport();
+            local.DataSources.Add(reportDataSource);
+
+            local.ReportPath = "Report/Sm1Report.rdlc";
+            local.EnableExternalImages = true;
+
+            double price = sample.Sum(d => d.LastTaqeem);
+
+            ToWord toWord = new ToWord((decimal)price, new CurrencyInfo(CurrencyInfo.Currencies.SaudiArabia));
+            ////get name
+            //string muthmenname = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Muthmen)?.EmployName;
+            //string aduitname = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Adutit)?.EmployName;
+            //string appovename = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Approver)?.EmployName;
+            //// get member id
+            //string muthmenid = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Muthmen)?.MemberId;
+            //string aduitid = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Adutit)?.MemberId;
+            //string appoveid = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Approver)?.MemberId;
+            ////get image sign
+            //string muthminsign = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Muthmen)?.SigImage;
+            //string auditsign = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Adutit)?.SigImage;
+            //string approvesign = _context.Users.SingleOrDefault(d => d.Id == treatments.SingleOrDefault().Approver)?.SigImage;
+            ////get image url
+            //string sigurlmuthmen = "http://" + HttpContext.Request.Host + "/ProfPic/" + muthminsign + ".jpg";
+            //string sigurlauditsign = "http://" + HttpContext.Request.Host + "/ProfPic/" + auditsign + ".jpg";
+            //string sigurlapprovesign = "http://" + HttpContext.Request.Host + "/ProfPic/" + approvesign + ".jpg";
+
+            ReportParameter[] parameters = {
+            //    new ReportParameter("sample",sample),
+            //    new ReportParameter("custmer",custmer),
+            //    new ReportParameter("muthmen", muthmenname),
+            //     new ReportParameter("audit", aduitname),
+            //      new ReportParameter("approver", appovename),
+                new ReportParameter("totprice",  toWord.ConvertToArabic()),
+
+
+                //    new ReportParameter("muthminsign",  sigurlmuthmen),
+                //    new ReportParameter("Auditsign",  sigurlauditsign),
+                //    new ReportParameter("Approvesign", sigurlapprovesign),
+
+
+                //    new ReportParameter("idmuthmin",  muthmenid),
+                //    new ReportParameter("idaudit",  aduitid),
+                //    new ReportParameter("idapprove", appoveid),
+                //    new ReportParameter("earthmap",  ""),
+                //    new ReportParameter("map", ""),
+                //    new ReportParameter("zoommap", ""),
+                //    new ReportParameter("images",images)
+
+
+                   };
+
+            local.SetParameters(parameters);
+
+            return local.Render("Pdf", "");
+        }
+
+
         public IActionResult Create()
         {
             ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Id");
